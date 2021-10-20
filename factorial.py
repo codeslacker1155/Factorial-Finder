2c0c6bc0443e5116cea7bbc23fdbfc2f8a01dea0
Chris H <47655454+codeslacker1155@users.noreply.github.com>
2021-10-20 04:40:21 -0400

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,14 @@
+def fac(n):
+    if(n==0):
+        return 1
+    elif(n==1):
+        return 1
+    else:
+        return n * fac(n-1)
+    
+t = input()
+while(t):
+    t=t-1
+    n = input()
+    p = fac(n)
+    print p
